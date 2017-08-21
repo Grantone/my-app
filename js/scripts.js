6315f2b2e6d08ffe8f9a12e855dbe9412061ec49
Granton Mchana <gmchana254@gmail.com>
2017-08-21 17:11:57 +0300

    adding scripts in project

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function() {
+  $("form#ping-pong").submit(function(event) {
+    event.preventDefault()
+    var numb =parseInt($("input#number").val());
+    console.log(numb);
+    countr(numb)
+  });
+});
+
+var countr = function(numb) {
+  for (var i = 1; i <= numb; i++) {
+  if ((i % 3 === 0) && (i % 5 === 0)) {
+
+  $("outputlist").append("<li>ping pong<li>");
+
+}
+else if (i % 5 === 0) {
+  alert("pong");
+}
+else if (i % 3 === 0) {
+  alert("ping");
+}
+else {
+  alert(i);
+}
+}
+};
