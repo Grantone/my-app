7352e79d500e2979fb5d8b3e3a3d6955e65b24a6
Granton Mchana <gmchana254@gmail.com>
2017-08-25 08:28:35 +0300

    changes added in index.html and in js contents

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,24 @@
 jQuery(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault()
-    var numb =parseInt($("input#number").val());
-    console.log(numb);
+    var numb = parseInt($("input#number").val());
     countr(numb)
   });
 });
 
 var countr = function(numb) {
+  var output = []
   for (var i = 1; i <= numb; i++) {
-  if ((i % 3 === 0) && (i % 5 === 0)) {
-
-  $("outp").append("<li>ping pong<li>");
-
-}
-else if (i % 5 === 0) {
-  alert("pong");
-}
-else if (i % 3 === 0) {
-  alert("ping");
-}
-else {
-  alert(i);
-}
-}
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+      output.push("ping pong")
+    } else if (i % 5 === 0) {
+      output.push(", pong")
+    } else if (i % 3 === 0) {
+      output.push(", ping")
+    } else {
+      output.push(i)
+    }
+  }
+  alert(output);
+  $("#waaa").append("<li>" + output +"</li>");
 };
